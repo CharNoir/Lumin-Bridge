d2b4faf7f67285ec26f4ff6c68f4b521f9ec36b4
CharNoir <59160057+CharNoir@users.noreply.github.com>
2025-04-08 17:08:41 +0200

    HdrChecker
    HdrChecker .NET 8 project to use DXGI to get hdr status

---
+++ HdrChecker/HdrMonitorInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HdrChecker
+{
+    public class HdrMonitorInfo
+    {
+        public string DeviceName { get; set; }
+        public bool HdrEnabled { get; set; }
+        public string ColorSpace { get; set; }
+    }
+}
---
+++ HdrChecker/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Management;
+using System.Text.Json;
+using HdrChecker;
+using Vortice.DXGI;
+using static Vortice.DXGI.DXGI;
+
+internal class Program
+{
+    static void Main()
+    {
+        var monitors = new List<HdrMonitorInfo>();
+
+        using IDXGIFactory6 factory = CreateDXGIFactory2<IDXGIFactory6>(debug: true);
+
+        uint adapterIndex = 0;
+        while (factory.EnumAdapterByGpuPreference(adapterIndex++, GpuPreference.Unspecified, out IDXGIAdapter1 adapter).Success)
+        {
+            uint outputIndex = 0;
+            while (adapter.EnumOutputs(outputIndex++, out IDXGIOutput output).Success)
+            {
+                if (output.QueryInterfaceOrNull<IDXGIOutput6>() is IDXGIOutput6 output6)
+                {
+                    var desc1 = output6.Description1;
+
+                    string deviceName = desc1.DeviceName;
+                    bool hdrEnabled = desc1.ColorSpace == ColorSpaceType.RgbFullG2084NoneP2020;
+
+                    monitors.Add(new HdrMonitorInfo
+                    {
+                        DeviceName = deviceName,
+                        HdrEnabled = hdrEnabled,
+                        ColorSpace = desc1.ColorSpace.ToString()
+                    });
+                }
+            }
+        }
+
+        Console.WriteLine(JsonSerializer.Serialize(monitors, new JsonSerializerOptions
+        {
+            WriteIndented = true
+        }));
+    }
+}
